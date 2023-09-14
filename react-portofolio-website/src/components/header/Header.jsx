7bd121f4de4472b33b2c7960679bf3cf7964a6d2
Gusnand <gusnanda1170@gmail.com>
2023-09-14 10:04:54 +0800

    header

--- react-portofolio-website/src/components/header/Header.jsx
+++ react-portofolio-website/src/components/header/Header.jsx
@@ -1,8 +1,25 @@
 import React from "react";
 import "./header.css";
+import CTA from "./CTA";
+import ME from "../../assets/me.png";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header>
+      <div className="container header__container">
+        <h5>Hello I'm</h5>
+        <h1>Gus Nand</h1>
+        <h5 className="text-light">Tech Enthusiasts</h5>
+        <CTA />
+        <div className="me">
+          <img src={ME} alt="Me" />
+        </div>
+        <a href="#contact" className="scroll__down">
+          Scroll Down
+        </a>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
---
+++ react-portofolio-website/src/components/header/HeaderSocials.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const HeaderSocials = () => {
+  return (
+    <div className="header__socials">
+      <a href="https://linkedin.com/ketutaguscahyadinanda/" target="_blank"></a>
+      <a href="https://github.com/Gusnand" target="_blank"></a>
+      <a href="https://instagram.com/floa.tint117" target="_blank"></a>
+    </div>
+  );
+};
+
+export default HeaderSocials;
