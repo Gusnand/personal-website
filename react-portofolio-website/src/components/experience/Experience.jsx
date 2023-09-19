90ccf0790b0c8d27395fbe1af28930ded5bf4a94
Gusnand <gusnanda1170@gmail.com>
2023-09-20 01:19:43 +0800

    added services component

--- react-portofolio-website/src/components/experience/Experience.jsx
+++ react-portofolio-website/src/components/experience/Experience.jsx
@@ -13,28 +13,46 @@ const Experience = () => {
           <h3>FrontEnd Dev.</h3>
           <div className="experience__content">
             <article className="experience__details">
-              <VerifiedIcon />
+              <VerifiedIcon className="experience__details-icon" />
               <div>
                 <h4>HTML</h4>
                 <small className="text-light">Experienced</small>
               </div>
             </article>
             <article className="experience__details">
-              <VerifiedIcon />
+              <VerifiedIcon className="experience__details-icon" />
               <div>
                 <h4>CSS</h4>
                 <small className="text-light">Experienced</small>
               </div>
             </article>
             <article className="experience__details">
-              <VerifiedIcon />
-              <h4>JavaScript</h4>
-              <small className="text-light">Experienced</small>
+              <VerifiedIcon className="experience__details-icon" />
+              <div>
+                <h4>JavaScript</h4>
+                <small className="text-light">Experienced</small>
+              </div>
             </article>
             <article className="experience__details">
-              <VerifiedIcon />
-              <h4>ReactJS</h4>
-              <small className="text-light">Moderate</small>
+              <VerifiedIcon className="experience__details-icon" />
+              <div>
+                <h4>ReactJS</h4>
+                <small className="text-light">Moderate</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <VerifiedIcon className="experience__details-icon" />
+              <div>
+                <h4>NextJS</h4>
+                <small className="text-light">Moderate</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <VerifiedIcon className="experience__details-icon" />
+              <div>
+                <h4>CSS Framework</h4>
+                <small className="text-light">Moderate</small>
+              </div>
             </article>
           </div>
         </div>
@@ -44,29 +62,46 @@ const Experience = () => {
           <h3>Editor.</h3>
           <div className="experience__content">
             <article className="experience__details">
-              <VerifiedIcon />
-              <h4>Adobe Premiere Pro CC</h4>
-              <small className="text-light">Expert</small>
+              <VerifiedIcon className="experience__details-icon" />
+              <div>
+                <h4>Premiere Pro</h4>
+                <small className="text-light">Expert</small>
+              </div>
             </article>
             <article className="experience__details">
-              <VerifiedIcon />
-              <h4>Adobe After Effects CC</h4>
-              <small className="text-light">Expert</small>
+              <VerifiedIcon className="experience__details-icon" />
+              <div>
+                <h4>After Effects</h4>
+                <small className="text-light">Expert</small>
+              </div>
             </article>
             <article className="experience__details">
-              <VerifiedIcon />
-              <h4>Figma</h4>
-              <small className="text-light">Expert</small>
+              <VerifiedIcon className="experience__details-icon" />
+              <div>
+                <h4>Figma</h4>
+                <small className="text-light">Expert</small>
+              </div>
             </article>
             <article className="experience__details">
-              <VerifiedIcon />
-              <h4>Adobe Photoshop CC</h4>
-              <small className="text-light">Expert</small>
+              <VerifiedIcon className="experience__details-icon" />
+              <div>
+                <h4>Photoshop</h4>
+                <small className="text-light">Expert</small>
+              </div>
             </article>
             <article className="experience__details">
-              <VerifiedIcon />
-              <h4>Corel Draw</h4>
-              <small className="text-light">Expert</small>
+              <VerifiedIcon className="experience__details-icon" />
+              <div>
+                <h4>Corel Draw</h4>
+                <small className="text-light">Expert</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <VerifiedIcon className="experience__details-icon" />
+              <div>
+                <h4>Lightroom</h4>
+                <small className="text-light">Expert</small>
+              </div>
             </article>
           </div>
         </div>
